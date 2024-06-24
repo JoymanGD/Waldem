12b86d4906da39c2b6dc95b4fe58441be21be838
JoymanGD <joyman.gamedev@gmail.com>
2024-06-24 18:48:23 +0300

    +key codes +mouse button codes

---
+++ Waldem/src/Waldem/MouseButtonCodes.h
@@ -0,0 +1,16 @@
+#pragma once
+
+//from glfw3.h
+
+#define WD_MOUSE_BUTTON_1         0
+#define WD_MOUSE_BUTTON_2         1
+#define WD_MOUSE_BUTTON_3         2
+#define WD_MOUSE_BUTTON_4         3
+#define WD_MOUSE_BUTTON_5         4
+#define WD_MOUSE_BUTTON_6         5
+#define WD_MOUSE_BUTTON_7         6
+#define WD_MOUSE_BUTTON_8         7
+#define WD_MOUSE_BUTTON_LAST      WD_MOUSE_BUTTON_8
+#define WD_MOUSE_BUTTON_LEFT      WD_MOUSE_BUTTON_1
+#define WD_MOUSE_BUTTON_RIGHT     WD_MOUSE_BUTTON_2
+#define WD_MOUSE_BUTTON_MIDDLE    WD_MOUSE_BUTTON_3
