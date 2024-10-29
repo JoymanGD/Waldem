47cb8ea74fcb45de7d0ee02ac95474ce07abfa3e
JoymanGD <joyman.gamedev@gmail.com>
2024-10-29 23:37:23 +0200

    [FIX] compilation errors fix

--- Waldem/src/Waldem/Renderer/Model/Mesh.cpp
+++ Waldem/src/Waldem/Renderer/Model/Mesh.cpp
@@ -6,7 +6,6 @@ namespace Waldem
 	Mesh::Mesh(void* vertexBufferData, uint32_t vertexBufferDataSize, uint32_t* indices, uint32_t indicesAmount, const BufferLayout& layout, Material material)
 	{
 		VB = VertexBuffer::Create(vertexBufferData, vertexBufferDataSize);
-		VB->SetLayout(layout);
 		IB = IndexBuffer::Create(indices, indicesAmount);
 		IB->SetIndices(indices);
 
