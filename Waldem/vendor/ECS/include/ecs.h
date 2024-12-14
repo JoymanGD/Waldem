44454e9c1c2784bc0c64167c29f07f087ddb57be
JoymanGD <joyman.gamedev@gmail.com>
2024-12-14 17:50:27 +0200

    [FIX] ecs fix for entity not being collected by ecsmanager when using EntitiesWith

--- Waldem/vendor/ECS/include/ecs.h
+++ Waldem/vendor/ECS/include/ecs.h
@@ -1005,9 +1005,8 @@ private:
 				if (pool_is_null) {
 					return false;
 				}
-				bool has_all_components{
-					(std::get<Pool<Ts>*>(pools_)->template Pool<Ts>::Has(entity) && ...)
-				};
+				auto result = (std::get<Pool<Ts>*>(pools_)->template Has(entity) && ...);
+				bool has_all_components = { result };
 				return has_all_components;
 			}
 
