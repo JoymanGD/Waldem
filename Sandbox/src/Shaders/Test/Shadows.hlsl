7a843ed33015fb7c85c9a3745db51f7bd10ede23
JoymanGD <joyman.gamedev@gmail.com>
2024-12-17 22:10:38 +0200

    [FIX] shadow acne fixed with normal offset bias

--- Sandbox/src/Shaders/Test/Shadows.hlsl
+++ Sandbox/src/Shaders/Test/Shadows.hlsl
@@ -1,17 +1,17 @@
-#include "Core.hlsl"
 #define SHADOW_BIAS 0.005f
+#define NORMAL_OFFSET_BIAS 0.3f
 
-float CalculateShadowFactor(Texture2D<float> Shadowmap, SamplerComparisonState cmpSampler, float3 worldPos, float4x4 view, float4x4 projection, float bias)
+float CalculateShadowFactor(Texture2D<float> Shadowmap, SamplerComparisonState cmpSampler, float4 worldPos, float3 normal, float4x4 view, float4x4 projection)
 {
-    float4 lightClipPos = mul(view, float4(worldPos, 1.0f));
-    lightClipPos = mul(projection, lightClipPos);
-
-    lightClipPos.xyz /= lightClipPos.w;
+    float4 worldPosFixed = worldPos + float4(normal, 0) * NORMAL_OFFSET_BIAS; //Normal offset bias to fix shadow acne
+    float4 lightClipPos = mul(view, worldPosFixed); //view space
+    lightClipPos = mul(projection, lightClipPos); //clip space
+    lightClipPos.xyz /= lightClipPos.w; //NDC
     
     float2 shadowUV = lightClipPos.xy * 0.5f + 0.5f;
     shadowUV.y = -shadowUV.y;
 
-    float depth = lightClipPos.z - bias;
+    float depth = lightClipPos.z;
 
     float sum = 0;
     
