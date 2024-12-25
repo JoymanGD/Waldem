87714aec8ae49110589986553b65034c42828cc7
JoymanGD <joyman.gamedev@gmail.com>
2024-12-25 13:03:10 +0200

    [ADD] NoPCH filter for imgui and imguizmo source files

--- Waldem/vendor/ImGui/imgui_tables.cpp
+++ Waldem/vendor/ImGui/imgui_tables.cpp
@@ -187,7 +187,6 @@ Index of this file:
 // [SECTION] Header mess
 //-----------------------------------------------------------------------------
 
-#include <wdpch.h>
 #if defined(_MSC_VER) && !defined(_CRT_SECURE_NO_WARNINGS)
 #define _CRT_SECURE_NO_WARNINGS
 #endif
