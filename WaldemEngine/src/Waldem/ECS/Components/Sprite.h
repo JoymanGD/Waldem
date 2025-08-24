b1bcd757e39d5ab7350bd1cc165c5e319abe438a
JoymanGD <joyman.gamedev@gmail.com>
2025-08-24 18:50:57 +0300

    [FIX] sprite component data layout fixed for now

--- WaldemEngine/src/Waldem/ECS/Components/Sprite.h
+++ WaldemEngine/src/Waldem/ECS/Components/Sprite.h
@@ -6,13 +6,12 @@ namespace Waldem
     struct Sprite
     {
         COMPONENT(Sprite)
-            FIELD(AssetReference, Texture)
             FIELD(Vector4, Color)
-            FIELD(Vector4, Rect)
+            FIELD(AssetReference, Texture)
         END_COMPONENT()
         
-        TextureReference TextureRef;
         Vector4 Color = Vector4(1,1,1,1); // tint
+        TextureReference TextureRef;
         Vector4 Rect = Vector4(0,0,1,1); // UV region in atlas
     };
 }
